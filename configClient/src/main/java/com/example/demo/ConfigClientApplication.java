efd23f6696be628a8186b8a077c9acc11f4fcdc3
willwill1101 <willwill1101@aliyun.com>
2018-04-17 16:25:14 +0800

    测试提交一次

--- configClient/src/main/java/com/example/demo/ConfigClientApplication.java
+++ configClient/src/main/java/com/example/demo/ConfigClientApplication.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class ConfigClientApplication {
+public class ConfigClientApplication { 
 
     public static void main(String[] args) {
         SpringApplication.run(ConfigClientApplication.class, args);
